9603dc1d77aa84a3cc889ecb3e9c705c60e8d25d
Ahn-Hyun <pahn@dasomx.com>
2026-07-11 11:01:12 +0900

    Support MinIO storage for self-hosting

--- apps/web/src/server/storage/index.ts
+++ apps/web/src/server/storage/index.ts
@@ -1,7 +1,9 @@
-import { cloudflare } from "@better-upload/server/clients";
+import { cloudflare, minio } from "@better-upload/server/clients";
 
-export const s3Client = cloudflare({
-  accountId: process.env.R2_ACCOUNT_ID!,
-  accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-  secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
-});
+export const s3Client = process.env.MINIO_ENDPOINT
+  ? minio()
+  : cloudflare({
+      accountId: process.env.R2_ACCOUNT_ID!,
+      accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+    });
